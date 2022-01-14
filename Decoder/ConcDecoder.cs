d8fea584cd2165b9e2a923a6b94bb9854c38904e
FurkanK <73836618+Furkan1000174@users.noreply.github.com>
2022-01-14 18:49:29 +0100

    Done?
    Check if thread safe

--- Decoder/ConcDecoder.cs
+++ Decoder/ConcDecoder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Decoder;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace ConcDecoder
 {
@@ -10,10 +12,15 @@ namespace ConcDecoder
     public class ConcurrentTaskBuffer : TaskBuffer
     {
         //todo: add required fields such that satisfies a thread safe shared buffer.
-
+        private Mutex mutex;
+        private Semaphore provider_sem, worker_sem;
+        
         public ConcurrentTaskBuffer() : base()
         {
             //todo: implement this method such that satisfies a thread safe shared buffer.
+            this.mutex = new Mutex();
+            this.provider_sem = new Semaphore(1,1);
+            this.worker_sem = new Semaphore(0,1);
         }
 
         /// <summary>
@@ -23,6 +30,13 @@ namespace ConcDecoder
         public override void AddTask(TaskDecryption task)
         {
             //todo: implement this method such that satisfies a thread safe shared buffer.
+            lock(mutex){
+            this.taskBuffer.Enqueue(task);
+            this.numOfTasks++;
+            this.maxBuffSize = this.taskBuffer.Count > this.maxBuffSize ? this.taskBuffer.Count  : this.maxBuffSize;
+            this.LogVisualisation();
+            this.PrintBufferSize();
+            }
         }
 
         /// <summary>
@@ -33,7 +47,17 @@ namespace ConcDecoder
         {
             //todo: implement this method such that satisfies a thread safe shared buffer.
             TaskDecryption t = null;
-
+            lock(mutex){
+            if (this.taskBuffer.Count > 0)
+            {
+                t = this.taskBuffer.Dequeue();
+                // check if the task is the last ending task: put the task back.
+                // It is an indication to terminate processors
+                if (t.id < 0)
+                    this.taskBuffer.Enqueue(t);
+            }
+            //this.worker_sem.Release();
+            }
             return t;
         }
 
@@ -43,6 +67,9 @@ namespace ConcDecoder
         public override void PrintBufferSize()
         {
             //todo: implement this method such that satisfies a thread safe shared buffer.
+            lock(mutex){
+            Console.Write("Buffer#{0} ; ", this.taskBuffer.Count);
+            }
         }
     }
 
@@ -61,7 +88,35 @@ namespace ConcDecoder
             ConcurrentTaskBuffer tasks = new ConcurrentTaskBuffer();
 
             //todo: implement this method such that satisfies a thread safe shared buffer.
+            Provider[] providers = new Provider[numOfProviders];
+            Worker[] workers = new Worker[numOfWorkers];
+            LinkedList<Thread> threads = new LinkedList<Thread>();
 
+            for (int i = 0; i < numOfProviders; i++)
+                providers[i] = new Provider(tasks, this.challenges);
+
+            for (int i = 0; i < numOfWorkers; i++)
+                workers[i] = new Worker(tasks);
+
+            for (int i = 0; i < numOfProviders; i++)
+            {
+                //threads.AddFirst(new Thread(providers[i].SendTasks));
+                Thread provider = new Thread(providers[i].SendTasks);
+                threads.AddFirst(provider);
+            }
+            for (int i = 0; i < numOfWorkers; i++)
+            {
+                Thread worker = new Thread(workers[i].ExecuteTasks);
+                threads.AddFirst(worker);
+            }
+            foreach (Thread t in threads){
+                t.Start();
+            }
+            
+            Thread.Sleep(500);
+
+            foreach (Thread t in threads)
+                t.Join();
 
             return tasks.GetLogs();
         }
--- Decoder/Program.cs
+++ Decoder/Program.cs
@@ -8,13 +8,13 @@ namespace Decoder
     // The values within WorkingParams can change during the experiments. 
     class WorkingParams
     {
-        public const string studentNumberOne = ""; // This must be filled.
-        public const string studentNumberTwo = ""; // This must be filled. Keep it "" if you are working alone.
-        public const string classNumber = "INF2A"; // This must be filled. INF2A is just an example.
+        public const string studentNumberOne = "101157"; // This must be filled.
+        public const string studentNumberTwo = "1000174"; // This must be filled. Keep it "" if you are working alone.
+        public const string classNumber = "INF2C"; // This must be filled. INF2A is just an example.
 
         public const int numOfWorkers = 40; // how many workers are needed to keep the max size of the shared buffer between 50 - 100?
         public const int minSendIntervalTime = 50; // min sending interval time (in msec) by the provider
-        public const int maxSendIntervalTime = 500; // max sending interval time (in msec) by the provider
+        public const int maxSendIntervalTime = 100; // max sending interval time (in msec) by the provider
     }
 
     // The values of FixedParams must not change in the final submission.
@@ -45,7 +45,7 @@ namespace Decoder
             Stopwatch conSW = new Stopwatch();
 
             seqSW.Start();
-            new Launch().SequentialTaskExecution();
+            //new Launch().SequentialTaskExecution();
             seqSW.Stop();
 
             TimeSpan seqET = seqSW.Elapsed;
