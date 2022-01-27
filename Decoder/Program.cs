49ad2d6cec68c21efea61bfb5045d9792f2b2dd0
FurkanK <73836618+Furkan1000174@users.noreply.github.com>
2022-01-27 16:14:09 +0100

    Complete run

--- Decoder/Program.cs
+++ Decoder/Program.cs
@@ -45,7 +45,7 @@ namespace Decoder
             Stopwatch conSW = new Stopwatch();
 
             seqSW.Start();
-            //new Launch().SequentialTaskExecution();
+            new Launch().SequentialTaskExecution();
             seqSW.Stop();
 
             TimeSpan seqET = seqSW.Elapsed;
